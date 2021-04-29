bfb9179cc951fe01721e0cf8316cdd4fb4eb6189
Keksiuwu <76953110+Keksiuwu@users.noreply.github.com>
2021-04-29 15:55:50 +0200

    Update api.py

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ app.config["DEBUG"] = True
 @app.route('/images/<endpoint>', methods=['GET'])
 def api(endpoint):
     return f'https://cdn.keksi.me/images/{endpoint}/' + (str(random.choice(os.listdir(f"D:\\API\\{endpoint}"))))
+    print('Flask GIF API by Keksi#6969')
 
 
 app.run(host="0.0.0.0", port=8888)
